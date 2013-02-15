6a095e32244cac010585105a3bca986838d9e759
danehans <danehans@cisco.com>
2013-02-15 16:42:44 +0000

    added Swift backend, bind/registry_host, and auth_uri parameters

--- manifests/glance.pp
+++ manifests/glance.pp
@@ -13,11 +13,17 @@
 # [db_host] Host where DB resides. Required.
 # [glance_user_password] Password for glance auth user. Required.
 # [glance_db_password] Password for glance DB. Required.
+# [registry_host] Address for Glance API to use for contacting the registry service.  Defaults to '0.0.0.0'
+# [bind_address] Address to bind Glance API and Registry services to.  Defaults to '0.0.0.0'
 # [keystone_host] Host whre keystone is running. Optional. Defaults to '127.0.0.1'
 # [auth_uri] URI used for auth. Optional. Defaults to "http://${keystone_host}:5000/"
 # [db_type] Type of sql databse to use. Optional. Defaults to 'mysql'
 # [glance_db_user] Name of glance DB user. Optional. Defaults to 'glance'
 # [glance_db_dbname] Name of glance DB. Optional. Defaults to 'glance'
+# [glance_on_swift] Optional. Use Swift as a backend for storing Glance images.  Defaults to false
+# [swift_store_user] Optional. User to authenticate against the Swift authentication service.  Defaults to admin:admin
+# [swift_store_key] Optional. Auth key for the user authenticating against the Swift authentication service. Defaults to keystone_admin
+# [swift_store_auth_address] Optional. Address where the Swift authentication service lives. Defaults to "http://${keystone_host}:5000/v2.0/" 
 # [verbose] Log verbosely. Optional. Defaults to 'False'
 # [enabled] Used to indicate if the service should be active (true) or passive (false).
 #   Optional. Defaults to true
@@ -34,13 +40,19 @@ class openstack::glance (
   $db_host,
   $glance_user_password,
   $glance_db_password,
-  $keystone_host        = '127.0.0.1',
-  $auth_uri             = "http://127.0.0.1:5000/",
-  $db_type              = 'mysql',
-  $glance_db_user       = 'glance',
-  $glance_db_dbname     = 'glance',
-  $verbose              = 'False',
-  $enabled              = true
+  $registry_host            = '0.0.0.0',
+  $bind_address             = '0.0.0.0',
+  $keystone_host            = '127.0.0.1',
+  $auth_uri                 = "http://127.0.0.1:5000/",
+  $db_type                  = 'mysql',
+  $glance_db_user           = 'glance',
+  $glance_db_dbname         = 'glance',
+  $verbose                  = 'False',
+  $glance_on_swift          = false,
+  $swift_store_user	    = 'admin:admin',
+  $swift_store_key          = 'keystone_admin',
+  $swift_store_auth_address = "http://${keystone_host}:5000/v2.0/",
+  $enabled                  = true
 ) {
 
   # Configure the db string
@@ -57,9 +69,12 @@ class openstack::glance (
     auth_type         => 'keystone',
     auth_port         => '35357',
     auth_host         => $keystone_host,
+    auth_uri          => "http://${keystone_host}:35357",
     keystone_tenant   => 'services',
     keystone_user     => 'glance',
     keystone_password => $glance_user_password,
+    registry_host     => $registry_host,
+    bind_host         => $bind_address,
     sql_connection    => $sql_connection,
     enabled           => $enabled,
   }
@@ -74,11 +89,21 @@ class openstack::glance (
     keystone_tenant   => 'services',
     keystone_user     => 'glance',
     keystone_password => $glance_user_password,
+    auth_uri          => "http://${keystone_host}:35357",
+    bind_host         => $bind_address,
     sql_connection    => $sql_connection,
     enabled           => $enabled,
   }
 
   # Configure file storage backend
-  class { 'glance::backend::file': }
-
+  if $glance_on_swift {
+    class { 'glance::backend::swift':
+      swift_store_user                    => $swift_store_user,
+      swift_store_key                     => $swift_store_key,
+      swift_store_auth_address            => $swift_store_auth_address,
+      swift_store_create_container_on_put => 'true'
+    }
+  } else {
+    class { 'glance::backend::file': }
+  }
 }
